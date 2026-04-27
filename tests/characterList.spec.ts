a31a2d2358eaad02a84d3ed88f0769488447e467
Ridai Govinda Pombo <ridai.pombo@marks-and-spencer.com>
2026-04-27 16:36:13 -0300

    Add Character List test and Bearer Token test

---
+++ tests/characterList.spec.ts
@@ -0,0 +1,51 @@
+import { test, expect } from '@playwright/test'
+
+var token: string
+
+auth()
+validateCharList()
+
+function auth() {
+  test("Generate a token", async ({ request }) => {
+    const user: string = process.env.API_USER
+    const password: string = process.env.API_PWD
+
+    expect(user).toBeDefined()
+    expect(password).toBeDefined()
+
+    const tokenResponse = await request.post(
+      "https://adventurers-guild-api.vercel.app/api/auth/token",
+      {
+        data: { username: user, password: password }
+      }
+    )
+
+    const tokenResponseBody = await tokenResponse.json()
+
+    expect(tokenResponse.status()).toBe(200)
+
+    expect(tokenResponseBody.token).not.toBeUndefined()
+    expect(tokenResponseBody.token).not.toBe("")
+
+    // Save the token
+    token = "Bearer " + tokenResponseBody.token
+  })
+}
+
+function validateCharList() {
+  test("List characters", async ({ request }) => {
+    const charResponse = await request.get(
+      'https://adventurers-guild-api.vercel.app/api/characters',
+      {
+        headers: { Authorization: token },
+      },
+    )
+
+    const charResponseBody = await charResponse.json()
+
+    expect(charResponse.status()).toBe(200)
+
+    expect(charResponseBody).toHaveProperty("length")
+    expect(charResponseBody.length).toBeGreaterThan(2)
+  })
+}
